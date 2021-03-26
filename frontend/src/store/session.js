ebe070354a19081c4f5e86147eec6b6f800905da
josh <joshtupas@gmail.com>
2021-03-26 15:51:37 -0500

    Signup thunk action functional

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -18,6 +18,23 @@ const removeUser = () => {
   };
 };
 
+export const signup = (user) => async (dispatch) => {
+  const { username, email, password } = user;
+  const response = await csrfFetch('/api/users', {
+    method: 'POST',
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+
+  dispatch(setUser(data.user));
+
+  return response;
+};
+
 export const restoreUser = () => async dispatch => {
   const response = await csrfFetch('/api/session');
   const data = await response.json();
