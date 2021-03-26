945517eb1f8d658d0aae83af658b61b58808dbf5
josh <joshtupas@gmail.com>
2021-03-25 22:19:14 -0500

    Redux store setup functional

---
+++ frontend/src/index.js
@@ -0,0 +1,34 @@
+// NPM IMPORTS
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+// LOCAL IMPORTS
+import './index.css';
+import App from './App';
+import configureStore from './store';
+
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
